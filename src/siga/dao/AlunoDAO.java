813381836bd349ea1f238bcc124474a92ffe7e50
evandro <azevedoevandrogabriel@gmail.com>
2026-09-02 12:46:14 -0300

    Retirando dependencia circular encontra entre Aluno e Matricula

--- src/siga/dao/AlunoDAO.java
+++ src/siga/dao/AlunoDAO.java
@@ -2,21 +2,18 @@ package siga.dao;
 
 public class AlunoDAO {
     private String nome;
-    private String matricula;
     private String email;
     private double media;
     private boolean bolsista;
 
-    public AlunoDAO(String nome, String matricula, String email, double media, boolean bolsista) {
+    public AlunoDAO(String nome, String email, double media, boolean bolsista) {
         this.nome = nome;
-        this.matricula = matricula;
         this.email = email;
         this.media = media;
         this.bolsista = bolsista;
     }
 
     public String getNome()      { return nome; }
-    public String getMatricula() { return matricula; }
     public String getEmail()     { return email; }
     public double getMedia()     { return media; }
     public boolean isBolsista()  { return bolsista; }
